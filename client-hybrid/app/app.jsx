58c38ea7a94c0af7a347765dd5f69edbc6cd7051
Catalin Buruiana <catalin@enki.com>
2017-03-04 03:52:11 +0100

    implement bg geolocation base

--- client-hybrid/app/app.jsx
+++ client-hybrid/app/app.jsx
@@ -4,9 +4,28 @@ import ReactDOM from 'react-dom';
 function startApp() {
   ReactDOM.render(
     <div className="view">
-      <p>Hello, a</p>
+      <p>Hello, there</p>
     </div>, document.querySelector('.app'),
   );
+  const bgLocation = window.backgroundGeolocation;
+
+  const callbackFn = function (location) {
+    console.log('[js] BackgroundGeolocation callback:  ', location.latitude, ',', location.longitude);
+    bgLocation.finish();
+  };
+
+  const failureFn = function (error) {
+    console.log('BackgroundGeolocation error', error);
+  };
+
+  bgLocation.configure(callbackFn, failureFn, {
+    desiredAccuracy: 10,
+    stationaryRadius: 20,
+    distanceFilter: 30,
+    interval: 60000,
+  });
+
+  bgLocation.start();
 }
 
 if (window.cordova) {
