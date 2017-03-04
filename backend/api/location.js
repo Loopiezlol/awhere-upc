bc7b1f7d0c8e5b5740b45214bc7f44c846f602d1
Catalin Buruiana <catalin@enki.com>
2017-03-04 15:19:48 +0100

    please

--- backend/api/location.js
+++ backend/api/location.js
@@ -3,17 +3,24 @@ const wrap = require('co-express');
 const request = require('superagent');
 const keys = require('../keys');
 
-function getAirQualityData(latitude, longitude) {
+function* getAirQualityData(latitude, longitude) {
   console.log('calling with',
     `https://api.breezometer.com/baqi/?lat=${latitude.toPrecision(7)}&lon=${longitude.toPrecision(7)}&key=${keys.airQuality}`);
-  request.get(`http://api.airvisual.com/v1/nearest?lat=${latitude.toPrecision(7)}&lon=${longitude.toPrecision(7)}&key=${keys.airQuality}`)
-  .end((err, res) => {
-    if (err) {
-      return err;
-    }
-    console.log('got', res.body.data);
-    return res.body.data;
-  });
+  const response = yield request.get(`http://api.airvisual.com/v1/nearest?lat=${latitude.toPrecision(7)}&lon=${longitude.toPrecision(7)}&key=${keys.airQuality}`);
+  // .end((err, res) => {
+  //   if (err) {
+  //     return err;
+  //   }
+  //   console.log('got', res.body.data);
+  //   return res.body.data;
+  // });
+  console.log(response);
+  if (response.body.data) {
+    return response.body.data;
+  }
+  return {
+    err: 'Something went wrong :(',
+  };
 }
 
 function* handleLocation(req, res) {
