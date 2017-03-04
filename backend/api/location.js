7245939d75d0fb1ccd9c2c1872345c296b130970
Catalin Buruiana <catalin@enki.com>
2017-03-04 16:40:47 +0100

    finish parse location + send notif data

--- backend/api/location.js
+++ backend/api/location.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const wrap = require('co-express');
 const request = require('superagent');
 const keys = require('../keys');
+const { parseLocationData } = require('../helpers');
 
 function* getAirQualityData(latitude, longitude) {
   const response = yield request.get(`http://api.airvisual.com/v1/nearest?lat=${latitude.toPrecision(7)}&lon=${longitude.toPrecision(7)}&key=${keys.airQuality}`);
@@ -14,40 +15,24 @@ function* getAirQualityData(latitude, longitude) {
 }
 
 function* handleLocation(req, res) {
-  // console.log(req.body);
   const toNotify = {};
   const { location, scenarios } = req.body;
   if (scenarios.indexOf('airQuality') !== -1) {
     const airQualityData = yield getAirQualityData(location.latitude, location.longitude);
     console.log('all data: ', airQualityData);
     if (!('err' in airQualityData)) {
-      const aqius = airQualityData.current.pollution.aqius;
-      switch (true) {
-        case (aqius >= 51 && aqius <= 100):
-          console.log('MODERATE AIR QUALITY');
-          // TODO: add to toNotify
-          break;
-        case (aqius >= 101 && aqius <= 150):
-          console.log('UNHEALTY FOR SENSITIVE GROUPS');
-          break;
-        case (aqius >= 151 && aqius <= 200):
-          console.log('UNHEALTY');
-          break;
-        case (aqius >= 201 && aqius <= 300):
-          console.log('VERY UNHEALTHY');
-          break;
-        case (aqius >= 301):
-          console.log('dude...');
-          break;
-        default:
-          console.log('safe bro');
-          break;
-      }
+      toNotify.airQuality = parseLocationData(airQualityData);
     }
+  }
+  if (Object.keys(toNotify).length !== 0) {
+    res.json({
+      status: 200,
+      toNotify,
+    });
   }
   res.json({
     status: 200,
-    ok: true,
+    message: 'Nothing to return',
   });
 }
 
